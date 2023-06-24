0888c0002b94ea1294a4538d0b4f3cf84440d416
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-06-24 09:37:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "Web Developer",
+      "Software Developer",
+      "Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
